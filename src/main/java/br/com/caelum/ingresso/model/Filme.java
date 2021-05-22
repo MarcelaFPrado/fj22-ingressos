7d4cc5659efd1317e4f8c219a16e973bd10f378d
Marcela <marcelinhaffc@gmail.com>
2021-05-22 11:48:42 -0300

    [] caelum sessao+filme

--- src/main/java/br/com/caelum/ingresso/model/Filme.java
+++ src/main/java/br/com/caelum/ingresso/model/Filme.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Duration;
 
 /**
@@ -19,6 +22,7 @@ public class Filme {
     private Duration duracao;
     private String genero;
     
+    private BigDecimal preco = BigDecimal.ZERO;
     
     
     /**
@@ -28,10 +32,11 @@ public class Filme {
 
     }
     
-    public Filme(String nome, Duration duracao, String genero) {
+    public Filme(String nome, Duration duracao, String genero, BigDecimal preco) {
 		this.nome = nome;
 		this.duracao = duracao;
 		this.genero = genero;
+		this.preco	=	preco;
 	}
 
 	public Integer getId() {
@@ -65,4 +70,18 @@ public class Filme {
     public void setGenero(String genero) {
         this.genero = genero;
     }
+
+	/**
+	 * @return the preco
+	 */
+	public BigDecimal getPreco() {
+		return 	preco.setScale(2,	RoundingMode.HALF_UP);
+	}
+
+	/**
+	 * @param preco the preco to set
+	 */
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 }
